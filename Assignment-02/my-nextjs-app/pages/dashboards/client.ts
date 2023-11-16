40c48b2ebb45032f64228188961a871cdeec505c
Moheb420 <mohebrahmani780@hotmail.com>
2023-11-16 12:31:58 +0100

    commited

--- Assignment-02/my-nextjs-app/pages/dashboards/client.ts
+++
@@ -1,6 +0,0 @@
-// export default interface Client {
-//     clientId: number;
-//     firstName: string;
-//     lastName: string;
-//     email: string;
-//   }
--- Assignment-02/my-nextjs-app/pages/dashboards/data.ts
+++
@@ -1,9 +0,0 @@
-// export default interface Data {
-//   userId: number;
-//   firstName: string;
-//   lastName: string;
-//   email: string;
-//   password: string;
-//   personalTrainerId: number; 
-//   accountType: string; 
-// }
