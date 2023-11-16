1750ebdf34aa6f4fe30bbd5f7a02b606a6fe2849
Moheb420 <mohebrahmani780@hotmail.com>
2023-11-16 11:55:23 +0100

    commited

--- Assignment-02/my-nextjs-app/pages/dashboards/data.ts
+++ Assignment-02/my-nextjs-app/pages/dashboards/data.ts
@@ -1,40 +1,45 @@
 // Define the TrainerData type
 export interface Data {
-    userId: number;
-    firstName: string;
-    lastName: string;
-    email: string;
-    password: string;
-    personalTrainerId: number; 
-    accountType: string; 
-  }
+  userId: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  personalTrainerId: number; 
+  accountType: string; 
+}
 
+export default Data;
 
-  export interface Exercise {
-    exerciseId: number;
-    name: string;
-    description: string;
-    sets: number;
-    repetitions: number;
-    time: string;
-    workoutProgramId: number;
-    personalTrainerId: number;
-  }
-  
-  export interface WorkoutProgram {
-    workoutProgramId: number;
-    name: string;
-    description: string;
-    exercises: Exercise[];
-    personalTrainerId: number,
-    clientId: number,
-  }
-  
-  
+export interface Exercise {
+  exerciseId: number;
+  name: string;
+  description: string;
+  sets: number;
+  repetitions: number;
+  time: string;
+  workoutProgramId: number;
+  personalTrainerId: number;
+}
 
-  export interface Client {
-    clientId: number;
-    firstName: string;
-    lastName: string;
-    email: string;
-  }
+export default Exercise;
+
+export interface WorkoutProgram {
+  workoutProgramId: number;
+  name: string;
+  description: string;
+  exercises: Exercise[];
+  personalTrainerId: number;
+  clientId: number;
+}
+
+export default WorkoutProgram;
+
+export interface Client {
+  clientId: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
+export default Client;
